d984aaacc8106e185ce01450aee2924d623834ba
ektor5 <ek5.chimenti@gmail.com>
2019-05-07 18:56:29 +0200

    add multi topic processing
    Usage: mvn exec:java -D exec.mainClass=com.mycompany.app.App -Dexec.args="10"

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,6 +5,12 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
 
 import java.util.Properties;
 
@@ -20,35 +26,66 @@ public class App
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		final Integer WINDOWSIZE = 10;
 
-		System.out.println( "Hello World!" );
+		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-		String topic = "lala";
+		Integer ntopics = Integer.parseInt(args[0]);
+		System.out.println( "Hello World! " +  Integer.toString(ntopics));
+
+		String element = "MyVariable";
 		String address = "localhost";
 
 		/* Consumer */
 
-		FlinkKafkaConsumer<String> myConsumer =
-			createStringConsumerForTopic(topic, address, "123");
+		ArrayList<FlinkKafkaConsumer<String>> myConsumerArray = new ArrayList<FlinkKafkaConsumer<String>>();
+		ArrayList<DataStream<String>> streamArray = new ArrayList<DataStream<String>>();
+		ArrayList<DataStream<String>> windowArray = new ArrayList<DataStream<String>>();
+		ArrayList<FlinkKafkaProducer<String>> myProducerArray = new ArrayList<FlinkKafkaProducer<String>>();
 
-		myConsumer.setStartFromLatest();
+		for (int i=0; i<ntopics; i++){
+			myConsumerArray.add( createStringConsumerForTopic(element+Integer.toString(i), address, "123"));
+			myConsumerArray.get(i).setStartFromLatest();
+			streamArray.add( env
+				.addSource(myConsumerArray.get(i)));
 
-		DataStream<String> stream = env.addSource(myConsumer);
+			windowArray.add( streamArray.get(i)
+				.map(new MapFunction<String,Float>(){
+					@Override
+					public Float map(String s) throws Exception {
+						return Float.parseFloat(s);
+					}
+				} )
+				// tumbling count window of 100 elements size
+				.windowAll(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+				// compute the sum 
+				.sum(0)
+				.map(new MapFunction<Float,String>(){
+					@Override
+					public String map(Float f) throws Exception {
+						return Float.toString(f);
+					}
+				} ));
 
-		/* Producer */
 
-		FlinkKafkaProducer<String> myProducer = createStringProducer(topic+"flinked",address+":9092");
-		myProducer.setWriteTimestampToKafka(true);
+			/* Producer */
 
-		windowMap lala = new windowMap(WINDOWSIZE);
-		stream.rebalance().map(lala).print();
+			myProducerArray.add( createStringProducer(element+Integer.toString(i)+"flinked",address+":9092"));
+			myProducerArray.get(i).setWriteTimestampToKafka(true);
+
+			//windowMap lala = new windowMap(WINDOWSIZE);
+			//stream.rebalance().map(lala).print();
 
 
-		stream.addSink(myProducer);
+			windowArray.get(i).addSink(myProducerArray.get(i));
+		}
 
 		/* Exec */
 
 		env.execute();
 	}
+	public long extractTimestamp(Long element, long previousElementTimestamp) {
+		return previousElementTimestamp;
+	}
+
 	public static FlinkKafkaConsumer<String> createStringConsumerForTopic(
 			String topic, String kafkaAddress, String kafkaGroup ) {
 
