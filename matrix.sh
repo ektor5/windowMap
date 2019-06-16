764bb8e329f1b25adb22b5919729bf4e27a74d07
ektor5 <ek5.chimenti@gmail.com>
2019-06-16 19:19:37 +0200

    rewrite matrix script

--- matrix.sh
+++ matrix.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
 set -e
-
-VARS=(    1       5      10    50   100    500  1000)
-RFR_RATE=(0.0001  0.001  0.01  0.1  1)
-RQS_RATE=(1       5     10   100  1000)
+VARS=(    5       10      25      50)
+RFR_RATE=(0.01    0.02	  0.05    0.1  )
+RQS_RATE=(100     100     100     100)
 RUN_TIME=60
 
 rm -f var_counting
 
-for i in ${VARS[@]}
+for j in `seq ${#VARS}`
 do
-	for r in ${RFR_RATE[@]}
-	do
+	i=${VARS[$j]}
+	r=${RFR_RATE[$j]}
+	s=${RQS_RATE[$j]}
 
-		for s in ${RQS_RATE[@]}
-		do
-			echo -e `date` - "var \t $i rfr \t $r \t rqs $s"
-			if ! ./start_test.sh $i $r $s $RUN_TIME
-			then echo try again
-				./start_test.sh $i $r $s $RUN_TIME
-			fi
-		done
-		
-	done
-	
+	echo -e `date` - "var \t $i rfr \t $r \t rqs $s"
+	if ! ./start_test.sh $i $r $s $RUN_TIME
+	then echo try again
+		./start_test.sh $i $r $s $RUN_TIME
+	fi
 done
 
 mv var_counting var_counting_`date +%y%m%d`.csv
